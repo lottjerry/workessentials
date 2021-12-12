43068c859799cb159a859c733be5fd2186753dbc
Jerry Lott <lottjerry@gmail.com>
2021-12-12 14:47:23 -0600

    boostrap alert working and password validation working

--- workessentials.py
+++ workessentials.py
@@ -1,37 +1,37 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
-app = Flask(__name__) 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
-# when using forms a secret key will protect against modifying cookies and cross site forgery attacks
-app.config['SECRET_KEY'] = 'e930ee077e44c99e60e391709c6690dc'
-
-# Dummy Posts
 posts = [
     {
         'author': 'Corey Schafer',
-        'title': 'Blog Post 1', 
+        'title': 'Blog Post 1',
         'content': 'First post content',
         'date_posted': 'April 20, 2018'
     },
     {
         'author': 'Jane Doe',
-        'title': 'Blog Post 2', 
+        'title': 'Blog Post 2',
         'content': 'Second post content',
-        'date_posted': 'April 10, 2018'
+        'date_posted': 'April 21, 2018'
     }
 ]
 
+
 @app.route("/")
-@app.route("/home") # You can add another route
+@app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
+
 
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
-@app.route("/register", methods=['GET','POST'])
+
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -39,10 +39,18 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
