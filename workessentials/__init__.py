0bc865bd1d41d145f466ea8f1527ca4020231747
Jerry Lott <lottjerry@gmail.com>
2021-12-16 22:37:38 -0600

    Small change

--- workessentials/__init__.py
+++ workessentials/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_manager, UserMixin, login_required, logout_user, current_user
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'thisissupposedtobesecret!'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
