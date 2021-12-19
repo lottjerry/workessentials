d15f0f85f6ad3fc007f10a73379c654b4154c71e
Jerry Lott <lottjerry@gmail.com>
2021-12-19 12:03:27 -0600

    2nd Complete Reset of Project

--- README.md
+++
@@ -1,1 +0,0 @@
-# workessentials
--- workessentials/routes.py
+++ workessentials/routes.py
@@ -1,65 +1,108 @@
-from flask import render_template, redirect, url_for
-import flask_login
-from flask_login import login_manager
-from flask_login.login_manager import LoginManager
-from werkzeug.utils import redirect
-from workessentials import app, db
-from workessentials.forms import LoginForm, RegisterForm
-from workessentials.models import User
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+import os
+import secrets
+from PIL import Image # Pillow is used for image editing
+from flask import render_template, url_for, flash, redirect, request
+from workessentials import app, db, bcrypt
+from workessentials.forms import RegistrationForm, LoginForm, UpdateAccountForm
+from workessentials.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
 
-login_manager = LoginManager()
-login_manager.login_view = 'login'
-login_manager.init_app(app)
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
 
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
     if form.validate_on_submit():
-        # Check if the user exists in the database
-        user = User.query.filter_by(username=form.username.data).first()
-        # If the user exists then check to see if the password match
-        if user:
-            if check_password_hash(user.password, form.password.data):
-                login_user(user, remember=form.remember.data)
-                return redirect(url_for('dashboard'))
-        return '<h1> Invalid username or password </h1>'
-
-    return render_template('login.html', form=form, title='Login')
-
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    form = RegisterForm()
-
-    if form.validate_on_submit():
-        hashed_password = generate_password_hash(form.password.data, method='sha256')
-        # Creates a new user giving the information from the sign up form
-        new_user = User(username=form.username.data, email=form.email.data, password=hashed_password)
-        # Add that user to the database and commit changes
-        db.session.add(new_user)
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8') #.decode('utf-8') is for whole string instead of bytes
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
         db.session.commit()
-        
-        return '<h1> New user has been created! </h1>'
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
 
-    return render_template('signup.html', form=form)
 
-@app.route('/dashboard')
-@login_required
-def dashboard():
-    return render_template('dashboard.html', name=current_user.username)
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first() # Checks if users with that email exists in the database
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next') # args is a dictionary dont access using [] use get('next') because if the key dosent exist it returns none
+            return redirect(next_page) if next_page else redirect(url_for('home')) 
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
-@app.route('/logout')
-@login_required
+
+@app.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('home'))
+
+
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename) # Grab both file name and file extension that the user upload use '_' to throw away 
+    picture_fn = random_hex + f_ext # gets random hex + uploaded file extension
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn) # app.root_path = full path up to our package directory 
+    # os.path.join make sure that all gets joined in one correct path
+
+    # Image resizing before uploading with Pillow (Check out Video for more in depth guide to Pillow)
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+
+@app.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account',
+                           image_file=image_file, form=form)
